b9cc2243219eebb8193b0af4467d9252718e222b
hdk731 <hideki.inoue.business@gmail.com>
2019-04-05 11:33:29 +0900

    update

--- client.py
+++ client.py
@@ -1,58 +1,17 @@
 import logging
 import socket
-import sys
 
-from socket import gethostname
 
+# initialize
 logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
+# set address, port.
+sv_addr = socket.gethostbyname(socket.gethostname())
+sv_port = 51000
 
 
-def ex_communication(sv_addr: str, sv_port: int):
-    """
-    client gets opposite client address from the server.
-    :param sv_addr: <str> server address
-    :param sv_port: <int> server port
-    """
-
-    # send UDP packet to server.
-    upd_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    upd_sock.sendto(gethostname(), (sv_addr, sv_port))
-    logger.info('connection to: {} {}'.format(sv_addr, sv_port))
-
-    while True:
-        # wait until server response
-        data, sv_addr = upd_sock.recvfrom(1024)
-        logger.info('connection from: %s', sv_addr)
-
-        # get opposing client
-        cl_addr, cl_port = opposite_access_point(data)
-        # send UDP packet to Opposing client
-        upd_sock.sendto(gethostname(), (cl_addr, cl_port))
-        logger.info('connection to: {} {}'.format(cl_addr, cl_port))
-
-        # wait until opposing client response
-        data, addr = upd_sock.recvfrom(1024)
-        logger.info('connection from: %s', addr)
-
-
-def access_point(args: object, addr: str = '127.0.0.1', port: int = 9999) -> object:
-    """
-    get access point info from arguments.
-    :param addr: <str> server address
-    :param port: <int> server port
-    :return: < > ip address/port
-    """
-
-    if len(args) >= 3:
-        addr, port = args[1], int(args[2])
-    elif len(args) == 2:
-        addr, port = addr, int(args[1])
-    else:
-        addr, port = addr, port
-    return addr, int(port)
-
-
-def opposite_access_point(data):
+def split_rcvmsg(data: str) -> object:
     """
     get Opposing access point info from datagram.
     :param data: <datagram> received datagram from server
@@ -63,6 +22,32 @@ def opposite_access_point(data):
     return addr, int(port)
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-    ex_communication(*access_point(sys.argv))
+def sndmsg(message: str) -> bytes:
+    """
+    remake address and port string.
+    :param message: <str> user input message
+    :return: <bytes> message body
+    """
+
+    return message.encode("UTF-8")
+
+
+# Internet, UPD.
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client.sendto(sndmsg(socket.gethostname()), (sv_addr, sv_port))
+logger.info('connection to: {} {}'.format(sv_addr, sv_port))
+
+# wait until server message.
+# buffer size is 1024 bytes.
+data, server = client.recvfrom(1024)
+logger.info('connection from: {}', server)
+
+# split received message from server.
+cl_addr, cl_port = split_rcvmsg(data)
+client.sendto(sndmsg("> This is dummy."), (cl_addr, cl_port))
+while True:
+    message = input("> ")
+    # send UDP packet to Opposing client
+    client.sendto(sndmsg(message), (cl_addr, cl_port))
+    logger.info('connection to: {} {}'.format(cl_addr, cl_port))
--- server.py
+++ server.py
@@ -1,70 +1,58 @@
 import logging
 import socket
-import sys
+
+
+# initialize
+con_clients = []
 
 logger = logging.getLogger()
-cl_address = []
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
+# set address, port.
+sv_addr = socket.gethostbyname(socket.gethostname())
+sv_port = 51000
 
 
-def ex_communication(sv_addr: str, sv_port: int):
-    """
-    Exchange each other client info .
-    :param sv_addr: <str> server address
-    :param sv_port: <int> server port
-    """
-
-    # socket bind a port.
-    upd_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    upd_sock.bind((sv_addr, sv_port))
-
-    while True:
-        # buffer size is 1024 bytes
-        data, cl_addr = upd_sock.recvfrom(1024)
-        logger.info('connection from: %s', cl_addr)
-        upd_sock.sendto(remake_access_point(cl_addr), cl_addr)
-        logger.info('connection to: %s', cl_addr)
-
-        cl_address.append(cl_addr)
-        if len(cl_address) >= 2:
-            # client A ⇔ client B
-            logger.info("server - send client info to: %s", cl_address[0])
-            upd_sock.sendto(remake_access_point(cl_address[1]), cl_address[0])
-            logger.info("server - send client info to: %s", cl_address[0])
-            upd_sock.sendto(remake_access_point(cl_address[0]), cl_address[1])
-
-            # delete client info
-            cl_address.pop(1)
-            cl_address.pop(0)
-
-
-def access_point(args: object, addr: str = '127.0.0.1', port: int = 9999) -> object:
-    """
-    get access point info from arguments.
-    :param addr: <str> server address
-    :param port: <int> server port
-    :return: < > ip address/port
-    """
-
-    if len(args) >= 3:
-        addr, port = args[1], int(args[2])
-    elif len(args) == 2:
-        addr, port = addr, int(args[1])
-    else:
-        addr, port = addr, port
-    return addr, int(port)
-
-
-def remake_access_point(data: object) -> bytes:
+def sndmsg(address: object) -> bytes:
     """
     remake address and port string.
-    :param data: <datagram> address and port
-    :return: < > ip address:port
+    :param address: <datagram> address and port
+    :return: <bytes> message body
     """
 
-    addr, port = data
-    return ':'.join([addr, str(port)]).encode("UTF-8")
+    ip, port = address
+    return ':'.join([ip, str(port)]).encode("UTF-8")
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-    ex_communication(*access_point(sys.argv))
+# Internet, UPD.
+# socket bind a port.
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((sv_addr, sv_port))
+
+while True:
+    # wait until client message.
+    # buffer size is 1024 bytes.
+    data, client = server.recvfrom(1024)
+    logger.info('connection from: {}', client)
+
+    # duplicate check
+    if client not in con_clients:
+        con_clients.append(client)
+
+    if len(con_clients) == 1:
+        # server.sendto('waiting connection...', client)
+        # logger.info('connection to: {}', client)
+        pass
+
+    elif len(con_clients) == 2:
+        # exchange address each other.
+        logger.info("server - send client info to: {}", con_clients[1])
+        server.sendto(sndmsg(con_clients[1]), con_clients[0])
+        logger.info("server - send client info to: {}", con_clients[0])
+        server.sendto(sndmsg(con_clients[0]), con_clients[1])
+
+        # clear client info
+        con_clients.clear()
+
+    else:
+        pass
