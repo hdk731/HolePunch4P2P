b1b787df94e4a668bca1fb47e48e5ca896e5efa1
hdk731 <hideki.inoue.business@gmail.com>
2019-04-05 13:10:55 +0900

    update

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 # set address, port.
 sv_addr = socket.gethostbyname(socket.gethostname())
-sv_port = 51000
+sv_port = 45001
 
 
 def sndmsg(address: object) -> bytes:
